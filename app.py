cacb25b4c7e4e09b7600e3f3e673af2ec2f67cc8
Harshika644 <harshikas621@gmail.com>
2025-07-12 02:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Sahaay!"
+
 @app.route('/lostfound')
 def lostfound():
-    return render_template('lostfound.html')
+    return render_template('lostfound.html')  # Make sure lostfound.html exists in the templates folder
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
